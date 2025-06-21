942c1f67d934781a66f618e69983cb85cd226f80
Matheus Oliveira <matheus.rodriguesoliveira@hotmail.com>
2025-06-21 19:58:41 -0300

    add icons

--- composeApp/src/commonMain/kotlin/com/example/moneytracker/model/Notification.kt
+++ composeApp/src/commonMain/kotlin/com/example/moneytracker/model/Notification.kt
@@ -1,5 +1,5 @@
-data class Notification(
-    val id: String = "",
+package com.example.moneytracker.model
+
 import kotlinx.datetime.Clock
 
 data class Notification(
@@ -13,10 +13,10 @@ data class Notification(
 )
 
 enum class NotificationType {
-    BUDGET_ALERT,      // Alertas de orçamento próximo do limite
-    SPENDING_INSIGHT,  // Insights sobre gastos
-    SAVING_GOAL,      // Metas de economia
-    BILL_REMINDER,    // Lembretes de contas
-    ACHIEVEMENT,      // Conquistas e marcos
-    TIP              // Dicas financeiras
-} 
+    BUDGET_ALERT,
+    SPENDING_INSIGHT,
+    SAVING_GOAL,
+    BILL_REMINDER,
+    ACHIEVEMENT,
+    TIP
+}
