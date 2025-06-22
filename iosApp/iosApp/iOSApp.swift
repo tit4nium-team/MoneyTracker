21c91c7ecd3128a5977a9faf23ed8c333d00efbc
Matheus Oliveira <matheus.rodriguesoliveira@hotmail.com>
2025-06-22 00:52:06 -0300

    Fix code

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -1,7 +1,12 @@
 import SwiftUI
+import Firebase // Add this line
 
 @main
 struct iOSApp: App {
+
+    init(){
+        FirebaseApp.configure()
+      }
     var body: some Scene {
         WindowGroup {
             ContentView()
